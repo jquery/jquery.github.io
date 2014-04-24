6105911835df562fc2162b79619e29dbc46f66eb
Jessica Duarte <jessynd@Jessicas-MacBook-Air.local>
2014-04-24 02:08:19 -0400

    successfully got all information needed from API and on the way to printing all data per repo in boxes.

--- scripts.js
+++ scripts.js
@@ -2,8 +2,9 @@ var gi = {};
 
 gi.init = function() {
 	console.log("yo yo");
-	gi.getRepos();
 	gi.getOrg();
+	gi.getRepos();
+	
 
 };	
 
@@ -13,8 +14,15 @@ gi.getOrg = function(){
 		type: 'GET',
 		dataType: 'jsonp',
 		success: function(data){
-			alert("yo!");
-			console.log(data);
+			gi.since = data.data.created_at;
+			gi.since = gi.since.split('-');
+			gi.since = $('<h3 class="since">').text("On GitHub Since " + gi.since[0]);
+			gi.followers = $('<h3>').text("followers " + data.data.followers);
+			gi.publicRepos = $('<h3>').text("Repos: " + data.data.public_repos);
+			gi.lastAction = $('<h3>').text("Last Active: " +data.data.updated_at);
+			$('header').append(gi.since, gi.publicRepos, gi.followers, gi.lastAction)
+
+
 		} //end success
 	}); //end ajax
 }; //end gi.getOrg
@@ -24,12 +32,18 @@ gi.getRepos = function(){
   		type : 'GET',
   		dataType : 'jsonp',
   		success: function(data){
-  			alert("yo");
-  			console.log(data);
+  		  console.log(data);
+  		  for (var i = 0; i < data.data.length; i++) {
+  				gi.repoNames = data.data[i].name;
+  				$(".repoCase").append("<h4>"+ gi.repoNames + "</h4>", 
+  					);
+
+
+  				
+  			};
+  			
   		}//end success
   }); //end ajax call 
-
-
 }; //gi.getRepos
 
 
