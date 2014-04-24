9ec86bc80deb3a0f9c2945557efa5c08e90728cb
Jessica Duarte <jessynd@Jessicas-MacBook-Air.local>
2014-04-24 09:31:12 -0400

    finalized API calls and pulled in possibly relevant information

--- scripts.js
+++ scripts.js
@@ -35,11 +35,25 @@ gi.getRepos = function(){
   		  console.log(data);
   		  for (var i = 0; i < data.data.length; i++) {
   				gi.repoNames = data.data[i].name;
-  				$(".repoCase").append("<h4>"+ gi.repoNames + "</h4>", 
-  					);
+  				gi.repoLink = data.data[i].html_url;
+  				gi.repoDesc = data.data[i].description;
+  				gi.repoForks = data.data[i].forks;
+  				gi.repoIssues = data.data[i].open_issues;
+  				gi.repoLang = data.data[i].language;
+  				gi.repoWatchers = data.data[i].watchers;
+  				gi.repoAge = data.data[i].created_at.split('-');
+  				gi.repoWebsite = data.data[i].homepage;
 
+  				$(".repoCase").append("<div class='box'><h4>"+ "<a href=" + gi.repoLink + ">" + gi.repoNames +  "</a></h4><p>" + gi.repoDesc + "</p><p>Forked: " + gi.repoForks + " times</p><p>Open Issues: " + gi.repoIssues + "</p><p>Language: " + gi.repoLang + "</p><p>Watchers: " + gi.repoWatchers + "</p><p>Since " + gi.repoAge[0] + "</p></div>")  ;
 
   				
+
+  	
+
+
+
+
+ 
   			};
   			
   		}//end success
