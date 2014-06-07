ffafd90f57706a4290759f9b1198ead22fb13441
Jessica Duarte <jessicanduarte@gmail.com>
2014-06-07 01:36:25 -0400

    spacing fixes

--- scripts.js
+++ scripts.js
@@ -6,47 +6,43 @@ gi.init = function() {
 	gi.getRepos();	
 };	
 
-gi.getRepos = function(){
-  $.ajax('https://api.github.com/orgs/jquery/repos?callback=what',{
-  		type : 'GET',
-  		dataType : 'jsonp',
-  		success: function(data){
-  		  //Sort data by # of forks in descending order
-  		  data.data.sort(function(a, b) {
-    	  if (a.forks == b.forks) {
-          	return a.forks == b.forks? 0 : +a.forks 
-          	> +b.forks ? 1 : -1;
-    	 	 };
-    		return +a.forks < +b.forks ? 1 : -1;
-		  });
-	      //Loop through array for relevant data
-  		  for (var i = 0; i < data.data.length; i++) {
-  			gi.repoNames = data.data[i].name;
-  			gi.repoLink = data.data[i].html_url;
-  			gi.repoDesc = data.data[i].description;
-  			gi.repoForks = data.data[i].forks;
-  			gi.repoIssues = data.data[i].open_issues;
-  			gi.repoLang = data.data[i].language;
-  			gi.repoWatchers = data.data[i].watchers;
-  			gi.repoAge = data.data[i].created_at.split('-');
-  			gi.repoWebsite = data.data[i].homepage;
-  			//Call function appending data to HTML
-  			gi.addHtml();
-  		   };
+gi.getRepos = function() {
+	$.ajax("https://api.github.com/orgs/jquery/repos?callback=what", {
+		type: 'GET',
+		dataType: 'jsonp',
+		success: function(data) {
+
+  		    // Sort data by # of forks in descending order
+  			data.data.sort(function(a, b) {		
+  		  		if (a.forks == b.forks) {
+  		  			return a.forks == b.forks? 0 : +a.forks 
+  		  			> +b.forks ? 1 : -1;
+  		  		};
+  		  		return +a.forks < +b.forks ? 1 : -1;
+  		  	});
+
+	      // Loop through array for relevant data and add to array of object
+	      	for (var i = 0; i < data.data.length; i++) {
+
+	      	gi.repos= [ {
+	      		name:  data.data[i].name,
+	      		link: data.data[i].html_url,
+	      		description: data.data[i].description,
+	      		forks: data.data[i].forks,
+	      		issues: data.data[i].open_issues,
+	      		language: data.data[i].language,
+	      		watchers: data.data[i].watchers,
+	      		date: data.data[i].created_at.split('-')[0],
+	      		website: data.data[i].homepage
+	      	} ];
+
+	      	// Append repos 
+	      	$.tmpl( "reposTemplate", gi.repos ).appendTo( ".repoCase" )
+  			};
   		}
-  });
+  	});
 };
 
-gi.addHtml = function(data){
-	$(".repoCase").append("<div class='box'><h4>"+ "<a href=" 
-		+ gi.repoLink + ">" + gi.repoNames +  "</a></h4><p>" 
-		+ gi.repoDesc + "</p><p>Forked: " + gi.repoForks 
-		+ " times</p><p>Open Issues: " + gi.repoIssues 
-		+ "</p><p>Language: " + gi.repoLang + "</p><p>Watchers: " 
-		+ gi.repoWatchers + "</p><p>Since " + gi.repoAge[0] 
-		+ "</p></div>");
-}
-
-$(function(){
-   gi.init();
+$(function() {
+	gi.init();
 });
