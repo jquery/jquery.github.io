40875657dfac5926fbeccef9f682bee59247785e
Jessica Duarte <jessicanduarte@gmail.com>
2014-06-07 01:30:31 -0400

    defined template html markup

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 var gi = {};
+gi.markup = "<div class='box'><h4><a href='${link}'>${name}</a></h4><p>${description}</p><p>Forked: ${forks}  times</p><p>Open Issues: ${issues}</p><p>Language: ${language}</p><p>Watchers: ${watchers}</p><p>Since ${date}</p><a href='${website}'>${website}</a></div>";	
 
 gi.init = function() {
 	gi.getRepos();
